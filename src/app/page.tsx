310b4f49e379f116951b4c7c68bd426bfda4f5f2
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-25 16:13:38 -0300

    feat: updated tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 
 import FloatingNav from "./components/floating-nav"
+import Hero from "./components/Hero"
 
 export default function Home() {
   return (
     <main className="bg-gray-50 dark:bg-gray-900 min-h-screen">
-      {/* <FloatingNav /> */}
-
+      <FloatingNav />
+      <Hero />
     </main>
   )
 }
