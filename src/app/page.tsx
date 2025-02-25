8f32e3aa8ac60b1ba34a431fe2840167ec2c6028
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-25 16:48:33 -0300

    feat: created about pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import FloatingNav from "./components/floating-nav"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 export default function Home() {
   return (
     <main className="bg-gray-50 dark:bg-gray-900 min-h-screen">
       <FloatingNav />
       <Hero />
+      <About />
     </main>
   )
 }
