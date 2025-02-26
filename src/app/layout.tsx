4c641c1fea065318f5b60fa23a9c38ecd3a02287
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-26 01:27:01 -0300

    feat: created toggle mode and custom scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import "./globals.css"
-import { Inter } from "next/font/google"
-import type React from "react"
+import "./globals.css";
+import { Inter } from "next/font/google";
+import type React from "react";
 import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/theme-provider"
-// import { ModeToggle } from "@/components/mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FaccinDEV",
-  description: "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
+  description:
+    "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
   openGraph: {
     title: "FaccinDEV",
-    description: "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
+    description:
+      "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
     url: "https://faccindev.pro",
     siteName: "FaccinDEV",
     images: [
       {
-        url: "/logo.png", 
+        url: "/logo.png",
         width: 1200,
         height: 630,
         alt: "Logo FaccinDEV",
@@ -28,27 +30,27 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "FaccinDEV",
-    description: "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
-    images: ["/logo.png"], 
+    description:
+      "Portfólio FaccinDEV - Desenvolvedor Full Stack com foco em Next.js e TypeScript.",
+    images: ["/logo.png"],
   },
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {/* <div className="fixed top-4 right-4 z-50">
+          <div className="fixed top-4 right-4 z-50">
             <ModeToggle />
-          </div> */}
+          </div>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
